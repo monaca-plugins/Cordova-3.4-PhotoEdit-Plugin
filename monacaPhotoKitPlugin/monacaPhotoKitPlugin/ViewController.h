81c69368a15540091a6f4d6db6863fc2372ea6e9
Kaustubh <Kaustubh.chavan@xoriant.com>
2014-06-09 18:56:24 +0530

    First Commit for PhotoEdit Plugin 9th June 2014

---
+++ monacaPhotoKitPlugin/monacaPhotoKitPlugin/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  monacaPhotoKitPlugin
+//
+//  Created by MountainLion on 09/06/14.
+//  Copyright (c) 2014 Xoriant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface ViewController : UIViewController
+
+
+@end
+
